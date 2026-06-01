5212454cdf8fdcc3acfc0e9629cb4b3e32eb18ba
mhimavamshi <himavamshi1212@gmail.com>
2026-06-01 10:53:24 +0530

    add COLOR action; add initial memory copied from DSL before execution

--- agent.js
+++ agent.js
@@ -3,11 +3,12 @@ class Agent {
     constructor(pos, dsl) {
         this.pos = pos;
         this.UNIT = 1;
+        this.color = "red";
 
         this.dsl = dsl;
         this.executionctx = new Executor(this);
 
-        this.memory = {};
+        this.memory = structuredClone(dsl.initialMemory);
         this.senses = {};
     }
 
@@ -27,18 +28,35 @@ class Agent {
         // console.log(`moved to ${JSON.stringify(this.pos)}`);
     }
 
+    setColor(data) {
+        this.color = data;
+    }
+
     executePending() {
         // get all this.executionctx.pending; map to same function name and directly execute
         const pending = this.executionctx.pending;
         // console.log(`executing: ${JSON.stringify(pending)}`);
 
         for (const action of pending) {
-            if (action.move) {
-                this.move(action.move);
-            }
+            this.executeAction(action);
         }
 
         this.executionctx.pending = [];
+    }
+
+    executeAction(action) {
+        switch(action.type) {
+            // even here, if we have action type mapped directly to this.func name, then just pass the args
+            // for example, this has this.action.type func and called with action.data
+            case "MOVE":
+                this.move(action.data);
+                break;
+            case "COLOR":
+                this.setColor(action.data);
+                break;
+            default:
+                throw new Error(`Unknown action: ${action.type}`);
+        }
     }
 
 }
